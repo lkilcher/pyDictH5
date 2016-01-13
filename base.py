ba704ed7a13ad2108d8cb7192afac8e083ecbd24
Levi Kilcher <Levi.Kilcher@nrel.gov>
2016-01-13 15:42:37 -0700

    Reorganize subset and append.

--- base.py
+++ base.py
@@ -41,9 +41,14 @@ import numpy.testing as nptest
 from copy import deepcopy
 from . import io
 
-indx_subset_valid = (slice, np.ndarray, list, int)
 
+class indexer(object):
 
+    def __init__(self, parent):
+        self.parent = parent
+
+    def __getitem__(self, indx):
+        return self.parent._subset(indx)
 
 
 def _equiv_dict(d1, d2, print_diff=False):
@@ -96,6 +101,21 @@ class data(dict):
 
     """
 
+    @property
+    def subset(self, ):
+        return indexer(self)
+
+    def _subset(self, indx):
+        out = self.__class__()
+        for nm in self:
+            if isinstance(self[nm], data):
+                out[nm] = self[nm]._subset(indx)
+            elif isinstance(self[nm], np.ndarray):
+                out[nm] = self[nm][indx]
+            else:
+                out[nm] = deepcopy(self[nm])
+        return out
+
     def __getitem__(self, indx):
         if '.' not in indx:
             return dict.__getitem__(self, indx)
@@ -107,6 +127,28 @@ class data(dict):
                 for ky in indx.split('.'):
                     tmp = dict.__getitem__(tmp, ky)
                 return tmp
+
+    def append(self, other, array_axis=0):
+        """
+        Append another PyCoDa data object to this one.  This method
+        assumes all arrays should be appended (concatenated) along
+        axis 0.
+
+        The appended object must have matching keys and values with
+        the same data types.
+
+        Overload this method to implement alternate appending schemes.
+        """
+        out = self.__class__()
+        for nm, dat in self.iteritems():
+            if isinstance(dat, np.ndarray):
+                out[nm] = np.concatenate((self[nm],
+                                          other[nm]),
+                                         array_axis=array_axis)
+            elif isinstance(dat, PropData):
+                assert self[nm] == other[nm], ""
+            else:
+                dat.append(other[nm], array_axis=array_axis)
 
     def __setitem__(self, indx, val):
         if not isinstance(indx, basestring):
@@ -244,6 +286,19 @@ class data(dict):
         io.hdf5_write(buf, self, chunks=chunks, compression=compression)
 
 
+class PropData(data):
+
+    def _subset(self, indx):
+        return self
+
+    def append(self, other, array_axis=0):
+        """
+        """
+        assert self == other, ("These data items cannot be joined because "
+                               "their properties do not match.")
+        return self
+
+
 class SpecData(data):
     """
     A class for storing spectral data.
@@ -253,161 +308,7 @@ class SpecData(data):
     pass
 
 
-class flat(data):
-    """
-    This class of data assumes that all data in this class have the
-    same shape in the first dimension. This class makes it possible to
-    slice, and sub-index the data within it from the object's top
-    level.
-
-    Notes
-    -----
-
-    For example, if 'dat' is defined as::
-
-        dat = flat()
-        dat.time = np.arange(10)
-        dat.u = 2 + 0.6 * np.arange(10)
-        dat.v = 0.3 * np.ones(10)
-        dat.w = 0.1 * np.ones(10)
-
-    The data within that structure can be sub-indexed by::
-
-        subdat = dat[:5]
-
-    Also, if you have a similarly defined data object::
-
-        dat2 = flat()
-        dat2.time = np.arange(10, 20)
-        dat2.u = 0.8 * np.arange(10)
-        dat2.v = 0.6 * np.ones(10)
-        dat2.w = 0.2 * np.ones(10)
-
-    One can join these data object by:
-
-        dat.append(dat2)
-
-    """
-
-    def empty_like(self, npt, array_creator=np.empty):
-        """
-        Create empty arrays with first dimension of length `npt`, with
-        other dimensions consistent with this data object.
-
-        `array_creator` may be used to specify the function that creates
-        the arrays (e.g. np.zeros, np.ones). The default is np.empty.
-
-        """
-        out = self.__class__()
-        for nm, dat in self.iteritems():
-            if isinstance(dat, np.ndarray):
-                shp = list(dat.shape)
-                shp[0] = npt
-                out[nm] = array_creator(shp, dtype=dat.dtype,)
-            elif hasattr(self, 'empty_like'):
-                out[nm] = dat.empty_like(npt, array_creator=array_creator)
-        return out
-
-    def __getitem__(self, indx):
-        if isinstance(indx, indx_subset_valid + (tuple, )):
-            return self.subset(indx)
-        else:
-            return data.__getitem__(self, indx)
-
-    def append(self, other):
-        """
-        Append another PyCoDa data object to this one.  This method
-        assumes all arrays should be appended (concatenated) along
-        axis 0.
-
-        The appended object must have matching keys and values with
-        the same data types.
-
-        Overload this method to implement alternate appending schemes.
-        """
-        for nm, dat in self.iteritems():
-            if isinstance(dat, np.ndarray):
-                self[nm] = np.concatenate((self[nm],
-                                           other[nm]),
-                                          axis=0)
-            else:
-                dat.append(other[nm])
-
-    def subset(self, inds, **kwargs):
-        """
-        Take a subset of this data.
-
-        Parameters
-        ----------
-        inds : {slice, ndarray, list, int}
-               The indexing object to use to subset the data.
-        **kwargs: name: index
-               For compound data types that contain sub-compound data
-               types, specify the indices of each sub-data with name:
-               index pairs. The 'name' is the name of the sub-data
-               field, and the indices should be like `inds`.
-        """
-        if (inds.__class__ is tuple and len(inds) == 2 and
-                isinstance(inds[0], indx_subset_valid) and
-                isinstance(inds[1], dict)):
-            return self.subset(inds[0], **inds[1])
-        out = self.__class__()
-        for nm in self:
-            if isinstance(self[nm], data):
-                if nm in kwargs:
-                    out[nm] = self[nm][kwargs[nm]]
-                elif isinstance(self[nm], flat):
-                    out[nm] = self[nm][inds]
-                #else:
-                    #print nm, self[nm].__class__
-            else:
-                out[nm] = self[nm][inds]
-        return out
-
-
-class TimeBased(data):
-    """
-    This class of data assumes that all data in an instance has the
-    same last dimension. This makes it possible to slice and sub-index
-    the data within it from the object's top level.
-
-    Notes
-    -----
-
-    For example, if 'dat' is defined as::
-
-        dat = TimeBased()
-        dat.time = np.arange(10)
-        dat.u = np.vstack(2 + 0.6 * np.arange(10),
-                          0.3 * np.ones(10),
-                          0.1 * np.ones(10))
-
-    The data within that structure can be sub-indexed by::
-
-        subdat = dat[:5]
-
-    Also, if you have a similarly defined data object::
-
-        dat2 = flat()
-        dat2.time = np.arange(10, 20)
-        dat2.u = np.vstack(0.8 * np.arange(10),
-                           0.6 * np.ones(10),
-                           0.2 * np.ones(10))
-
-    One can join these data object by:
-
-        dat.append(dat2)
-
-    """
-
-    def __getitem__(self, indx):
-        if isinstance(indx, indx_subset_valid + (tuple, )):
-            return self.subset(indx)
-        else:
-            return dict.__getitem__(self, indx)
-
-
-class tabular(flat):
+class tabular(data):
     """
     A class for holding tabular (e.g. 'spreadsheet') type data.
 
@@ -439,7 +340,7 @@ class tabular(flat):
         buf.close()
 
 
-class geodat(flat):
+class geodat(data):
     """
     A class for holding 'gis' type data.
 
@@ -459,4 +360,3 @@ class geodat(flat):
         if len(other_inds) > 0:
             return inds, other_inds
         return inds
-
