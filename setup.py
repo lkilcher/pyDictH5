8e18a50bfbe20609805163f271754d731e96187a
Levi Kilcher <levi.kilcher@gmail.com>
2018-04-03 10:00:49 -0600

    Fix version-string import in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from distutils.core import setup
-import pyDictH5._version as ver
+import os
+
+# Get the version info We do this to avoid importing __init__, which
+# depends on other packages that may not yet be installed.
+base_dir = os.path.abspath(os.path.dirname(__file__))
+version = {}
+with open(base_dir + "/pyDictH5/_version.py") as fp:
+    exec(fp.read(), version)
 
 setup(
-    name=ver.__package__,
-    version=ver.__version__,
+    name=version['__package__'],
+    version=version['__version__'],
     author='Levi Kilcher',
     author_email='levi.kilcher@nrel.gov',
     packages=['pyDictH5'],
+    install_requires=['h5py', 'numpy'],
 )
